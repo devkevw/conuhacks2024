2b46e37d66259891e33497259bcf770caed6fc3b
Kevin Wang <kevin.wang7@mail.mcgill.ca>
2024-01-20 13:35:45 -0500

    Insert 16x16 grid for drawing

---
+++ script.js
@@ -0,0 +1,19 @@
+// 16 x 16 grid of divs
+const container = document.querySelector(".container");
+const size = 16;
+
+// outer loop for columns
+for (var i = 0; i < size; i++){
+    var row = document.createElement("div");
+    row.classList.add("row")
+    row.style.display = "flex";
+
+    // inner loop for row 
+    for (var j = 0; j < size; j++){
+        var square = document.createElement("div");
+        square.classList.add("square");
+        row.appendChild(square);
+    }  
+    
+    container.appendChild(row);
+}
