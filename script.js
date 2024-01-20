0e2ab2fd9d47afec0e95664ff0f81e2db1d22dae
Kevin Wang <kevin.wang7@mail.mcgill.ca>
2024-01-20 17:27:01 -0500

    Add slider to adjust pixel sizes
    When slider is adjusted, the size of the pixels changes based on the
    specified dimensions.

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
-// 16 x 16 grid of divs
+// n x n grid of divs; default 16
 const container = document.querySelector(".container");
-const size = 16;
+let size = 16;
 
+// build grid
+function buildGrid() {
 // outer loop for columns
-for (var i = 0; i < size; i++){
-    var row = document.createElement("div");
-    row.classList.add("row")
-    row.style.display = "flex";
+    for (let i = 0; i < size; i++){
+        let row = document.createElement("div");
+        row.classList.add("row")
+        row.style.display = "flex";
 
-    // inner loop for row 
-    for (var j = 0; j < size; j++){
-        var square = document.createElement("div");
-        square.classList.add("square");
-        row.appendChild(square);
-    }  
-    
-    container.appendChild(row);
+        // inner loop for row 
+        for (let j = 0; j < size; j++){
+            let square = document.createElement("div");
+            square.classList.add("square");
+            row.appendChild(square);
+        }  
+        
+        container.appendChild(row);
+    }
 }
+buildGrid();
 
 // for hover trail
 function changeSquareColor() {
     this.classList.add('hovered');
 }
 
-var squares = document.querySelectorAll(".square");
+let squares = document.querySelectorAll(".square");
 
 squares.forEach(function(square) {
     square.addEventListener('mouseover', changeSquareColor);
@@ -41,3 +45,40 @@ function clear() {
 
 clearButton.addEventListener('click', clear);
 
+// slider
+let slider = document.querySelector(".slider")
+let sliderText = document.querySelector(".slider-text")
+
+ slider.textContent = `${this.value} x ${this.value}`;
+
+  slider.addEventListener("input", function() {
+    sliderText.textContent = `${this.value} x ${this.value}`;
+    
+    removeSquares();
+    size = this.value;
+    buildGrid();
+    squares = document.querySelectorAll(".square");
+
+    squares.forEach(function(square) {
+        square.addEventListener('mouseover', changeSquareColor);
+      });
+
+
+  });
+
+
+  function removeSquares() {
+    let squares = document.querySelectorAll(".square");
+    let rows = document.querySelectorAll(".row");
+
+    squares.forEach(function(square){
+        square.remove();
+    })
+
+    rows.forEach(function(row){
+        row.remove();
+    })
+  }
+
+
+  
