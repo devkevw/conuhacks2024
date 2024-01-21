56495841f16d325c7c76fb02daaf6bbf67f66c3e
Kevin Wang <kevin.wang7@mail.mcgill.ca>
2024-01-20 20:17:50 -0500

    Delete old js file

--- old.js
+++
@@ -1,8 +0,0 @@
-let rows = document.querySelectorAll('.row')
-function addSquares(num){
-  rows.forEach(function(row){
-      let square = document.createElement("div");
-      square.classList.add("square");
-      row.appendChild(square);
-  });
-}
